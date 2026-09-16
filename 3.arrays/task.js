9cacc1fa92c6f165fe2e689f01e163984563549f
nastya.smirnova <imagine505@mail.ru>
2026-09-16 14:43:40 +0400

    Выполнены задачи к лекции 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    let result;
+    if (arr1.length === arr2.length){
+        result = arr1.every((el, ind) => el === arr2[ind] && ind === arr2.indexOf(el));
+    }else {
+        result = false;
+    }
+    return result;
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let result = users.filter(user => user.gender === gender)
+    .reduce((acc,user, _, arr) => acc + user.age / arr.length, 0);
+    return result;
 }
