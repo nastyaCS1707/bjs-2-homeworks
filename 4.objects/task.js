3eb6b7deed3202b2e48039dc59f402a2b8c5ab3e
nastya.smirnova <imagine505@mail.ru>
2026-09-16 16:31:44 +0400

    Выполнена задача к лекции 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,32 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if('marks' in this){
+        this.marks.push(...marks);
+    }else {
+        console.log("Студент отчислен")
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if('marks' in this && this.marks.length != 0){
+        return this.marks.reduce((acc, mark, _, arr) => acc + mark / arr.length, 0);
+    } else {
+        return 0;
+    }
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason
 }
